d09edd1814eb7f9f71eb12aec1124d154d14a6ca
Moshe Simantov <ms@development.co.il>
2020-12-23 16:22:40 +0200

    fix release script

--- release.sh
+++ release.sh
@@ -1,9 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-pluginFolder=${1}
-message=${2}
+pluginName=${1}
+version=${2}
 
-cd "svn/$pluginFolder"
-cp -a "../../$pluginFolder/." "./trunk"
-svn ci -m "$message"
+if [[ $# -lt 2 ]] ; then
+    echo "Usage: ${0} [pluginName] [commitMessage]"
+    exit 0
+fi
+
+svnPath="svn/${pluginName}"
+
+mkdir -p svn
+rm -rf "${svnPath}"
+
+svn co "https://plugins.svn.wordpress.org/${pluginName}" "${svnPath}" -q
+svn del "${svnPath}/assets/"*
+svn del "${svnPath}/trunk/"*
+svn del "${svnPath}/tags/${version}/"*
+
+cp -a "./${pluginName}/assets/." "${svnPath}/assets"
+cp -a "./${pluginName}/trunk/." "${svnPath}/trunk"
+cp -a "./${pluginName}/trunk/." "${svnPath}/tags/${version}"
+
+
+svn add "${svnPath}/assets/"*
+svn add "${svnPath}/trunk/"*
+svn add "${svnPath}/tags/${version}/"*
+
+svn ci "${svnPath}" -m "v${version}"
